acc3aac9d9c1533e487dfaa401b19286768ae4bd
Mohamedamin-Mohamed <101759235+Mohamedamin-Mohamed@users.noreply.github.com>
2023-07-18 10:12:21 -0500

    class serves as an entry point of the Application

--- Driver.java
+++ Driver.java
@@ -1,5 +1,13 @@
 import javax.swing.SwingUtilities;
 
+/*
+ * Files: Driver.java          
+ * Author: Mohamedamin Mohamed          
+ * Contact mohamedamin204080@gmail.com  
+ * Created 07/18/2023                   
+ * Modified: 07/18/2023              
+ * Description:This class serves as an entry point of the Application
+ */
 public class Driver {
 
 	public static void main(String[] args) {
