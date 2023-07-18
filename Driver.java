47d6db8973d907b9287fb7156481b3bb92d38aa1
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2023-07-18 09:56:28 -0500

    class that serves as an entry point of the Application

---
+++ Driver.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()->{;
+		new CalculatorApp();
+		});
+
+	}
+
+}
